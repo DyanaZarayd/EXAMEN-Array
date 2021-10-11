d3dd00b60b3fb98671aacd6b276d96755871f86b
DyanaZarayd <91444235+DyanaZarayd@users.noreply.github.com>
2021-10-11 12:23:30 -0500

    Add files via upload

---
+++ CrudArrayList/MetodosSistema.java
@@ -0,0 +1,23 @@
+package crudArrayList;
+
+import crudvector.*;
+import java.util.*;
+
+public class MetodosSistema {
+    
+    public ArrayList<datosPersona> list = new ArrayList<>();
+
+    public void registrarPersona(datosPersona x) {
+        list.add(x);
+    } 
+    
+    public void eliminarPersona(String nombre, String codigo){
+        for (int i = 0; i < list.size(); i++) {
+            datosPersona dp = list.get(i);
+            if (nombre.equals(dp.nombres) && codigo.equals(dp.codigo)) {
+                list.remove(i);
+            }
+        }
+    }
+    
+}
